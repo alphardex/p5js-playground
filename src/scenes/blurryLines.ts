90713716e52d120c015b2aa1dbc7c419916249e2
alphardex <2582347430@qq.com>
2021-08-29 14:56:07 +0800

    update

--- src/scenes/blurryLines.ts
+++ src/scenes/blurryLines.ts
@@ -1,51 +1,87 @@
 import p5 from "p5";
 
+class PointLine {
+  s: p5;
+  x1: number;
+  y1: number;
+  x2: number;
+  y2: number;
+  d: number;
+  dotCount: number;
+  modifyFunc: Function;
+  constructor(s: p5, x1: number, y1: number, x2: number, y2: number, d = 6) {
+    this.s = s;
+    this.x1 = x1;
+    this.y1 = y1;
+    this.x2 = x2;
+    this.y2 = y2;
+    this.d = d;
+    let dotCount = s.dist(x1, y1, x2, y2);
+    this.dotCount = s.ceil(dotCount);
+    const modifyFunc = (x: number, y: number) => [x, y];
+    this.modifyFunc = modifyFunc;
+  }
+  draw() {
+    for (let i = 0; i < this.dotCount; i++) {
+      let x = this.s.map(i, 0, this.dotCount, this.x1, this.x2);
+      let y = this.s.map(i, 0, this.dotCount, this.y1, this.y2);
+      [x, y] = this.modifyFunc(x, y);
+      this.s.ellipse(x, y, this.d, this.d);
+    }
+  }
+  blur(seed = 0, amount = 0) {
+    const blurPower = 1 + this.s.sq(amount);
+    this.dotCount *= blurPower;
+    const blurPower2 = 1 - amount;
+    this.d *= blurPower2;
+    this.modifyFunc = (x: number, y: number) => {
+      x += seed * amount * this.s.randomGaussian(0, 1);
+      y += seed * amount * this.s.randomGaussian(0, 1);
+      return [x, y];
+    };
+  }
+}
+
+class PointShape extends PointLine {
+  x: number;
+  y: number;
+  r: number;
+  p: number;
+  lines: PointLine[];
+  constructor(s: p5, x: number, y: number, r: number, p: number) {
+    super(s, x, x, y, y);
+    this.x = x;
+    this.y = y;
+    this.r = r;
+    this.p = p;
+    this.lines = [];
+    for (let i = 0; i < this.p; i++) {
+      const x1 = this.x + this.r * this.s.cos((this.s.TWO_PI * i) / this.p);
+      const y1 = this.y + this.r * this.s.sin((this.s.TWO_PI * i) / this.p);
+      const x2 =
+        this.x + this.r * this.s.cos((this.s.TWO_PI * (i + 1)) / this.p);
+      const y2 =
+        this.y + this.r * this.s.sin((this.s.TWO_PI * (i + 1)) / this.p);
+      const line = new PointLine(this.s, x1, y1, x2, y2);
+      this.lines.push(line);
+    }
+  }
+  draw() {
+    this.lines.forEach((line) => {
+      line.draw();
+    });
+  }
+  blur(seed = 0, amount = 0) {
+    this.lines.forEach((line) => {
+      line.blur(seed, amount);
+    });
+  }
+}
+
 const sketch = (s: p5) => {
   let time = 0;
-
-  const blurryLine = (
-    x1: number,
-    y1: number,
-    x2: number,
-    y2: number,
-    seed = 0,
-    blur = 0
-  ) => {
-    let dotCount = s.dist(x1, y1, x2, y2);
-    const blurPower = 1 + s.sq(blur);
-    dotCount *= blurPower;
-    dotCount = s.ceil(dotCount);
-    let d = 6;
-    const blurPower2 = 1 - blur;
-    d *= blurPower2;
-    for (let i = 0; i < dotCount; i++) {
-      let x = s.map(i, 0, dotCount, x1, x2);
-      let y = s.map(i, 0, dotCount, y1, y2);
-      x += seed * blur * s.randomGaussian(0, 1);
-      y += seed * blur * s.randomGaussian(0, 1);
-      s.ellipse(x, y, d, d);
-    }
-  };
-
-  const blurryShape = (
-    x: number,
-    y: number,
-    r: number,
-    p = 3,
-    seed = 0,
-    blur = 0
-  ) => {
-    for (let i = 0; i < p; i++) {
-      blurryLine(
-        x + r * s.cos((s.TWO_PI * i) / p),
-        y + r * s.sin((s.TWO_PI * i) / p),
-        x + r * s.cos((s.TWO_PI * (i + 1)) / p),
-        y + r * s.sin((s.TWO_PI * (i + 1)) / p),
-        seed,
-        blur
-      );
-    }
-  };
+  let N = 12;
+  let velocity = 0.02;
 
   const setup = () => {
     s.createCanvas(s.windowWidth, s.windowHeight, s.WEBGL);
@@ -59,16 +95,21 @@ const sketch = (s: p5) => {
     s.background(0);
 
     s.push();
-    let N = 12;
     for (let i = 0; i < N; i++) {
-      s.fill(0.5 + i / N, 0.7, 0.25);
-      let offset = (s.PI * i) / N;
+      const ratio = i / N;
+
+      s.fill(0.5 + ratio, 0.7, 0.25);
+
+      const offset = s.PI * ratio;
       let x = 160 * s.cos(s.TWO_PI * time - offset);
-      blurryShape(x, 0, 90, 4, 10, i / N);
+
+      const shape = new PointShape(s, x, 0, 90, 6);
+      shape.blur(10, ratio);
+      shape.draw();
     }
     s.pop();
 
-    time += 0.02;
+    time += velocity;
   };
 
   s.setup = setup;
